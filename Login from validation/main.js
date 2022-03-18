127f2dca960007f2478481dd4a53f30a738c646b
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-03-19 02:30:35 +0600

    Validating username and password

--- Login from validation/main.js
+++ Login from validation/main.js
@@ -1,5 +1,7 @@
 // DOM Elements
 
+const container = document.querySelector('.login-container');
+const form = document.querySelector('.loginForm');
 const username = document.querySelector('#username');
 const password = document.querySelector('#password');
 const togglePasswordIcon = document.querySelector('i.fa-eye');
@@ -22,3 +24,14 @@ togglePasswordIcon.onclick = () => {
 // 	}
 // }
 // * * show hide password   * * \\
+
+// * * simple validation
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	if (username.value === '' || username.value.length < 3) {
+	}
+
+	if (password.value === '' || password.value.length < 6) {
+	}
+});
